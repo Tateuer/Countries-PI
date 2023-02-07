8ac83ebfd4bd2a42e88365bed86ce404b1a9032a
Tateuer <tateuer@gmail.com>
2023-02-07 13:25:26 -0300

    commit modularizado

---
+++ client/src/components/Filters.jsx
@@ -0,0 +1,97 @@
+import React from "react";
+import { useDispatch, useSelector } from "react-redux";
+import {
+  orderAlphabetical,
+  orderContinent,
+  orderPopulation,
+  byActivity,
+  // forActivity,
+} from "../redux/actions";
+
+const Filters = (props) => {
+  const dispatch = useDispatch();
+  const activities = useSelector((state) => state.activities);
+
+  function handleOrderAbc(e) {
+    e.preventDefault();
+    dispatch(orderAlphabetical(e.target.value));
+  }
+
+  function handlePopulation(e) {
+    e.preventDefault();
+    dispatch(orderPopulation(e.target.value));
+  }
+
+  function handleContinent(e) {
+    e.preventDefault();
+    dispatch(orderContinent(e.target.value));
+  }
+
+  function handleFilteredActivity(e) {
+    e.preventDefault();
+    dispatch(byActivity(e.target.value));
+  }
+  //  function handleFilteredActivitySeason(e){
+  //     e.preventDefault();
+  //     dispatch(forActivity(e.target.value))
+  //   }
+
+  return (
+    <div className="filtros">
+      <div className="ordertext">
+        <span className="inputfiltros">Order Alphabetical</span>
+        <select className="orderbutton" onChange={handleOrderAbc}>
+          <option value="ascendente">A-Z</option>
+          <option value="descendente">Z-A</option>
+        </select>
+      </div>
+      <div className="ordertext">
+        <span className="inputfiltros">Order Population</span>
+        <select className="orderbutton" onChange={handlePopulation}>
+          <option value="ascendente">Desc</option>
+          <option value="descendente">Asc</option>
+        </select>
+      </div>
+      <div className="ordertext">
+        <span className="inputfiltros">Order Continent</span>
+        <select className="orderbutton" onChange={handleContinent}>
+          <option value="all">All</option>
+          <option value="North America">North America</option>
+          <option value="South America">South America</option>
+          <option value="Antarctica">Antarctica</option>
+          <option value="Europe">Europe</option>
+          <option value="Asia">Asia</option>
+          <option value="Oceania">Oceania</option>
+        </select>
+      </div>
+      <div className="ordertext">
+        <span className="inputfiltros">Activities</span>
+        <select
+          className="orderbutton"
+          onChange={(e) => handleFilteredActivity(e)}
+        >
+          <option value="all">All</option>
+          {activities?.map((country) => {
+            return (
+              <option key={country.id} value={country.name}>
+                {country.name}
+              </option>
+            );
+          })}
+        </select>
+      </div>
+      {/* <div className="ordertext">
+        <span className="inputfiltros">Activities</span>
+          <select className="orderbutton" onChange= {(e) => handleFilteredActivitySeason(e)}>
+                <option value="all">All</option>
+                     {activities?.map((country) => {
+                       return <option key={country.id} value={country.season} >{country.season}</option>
+                    })}
+                    
+         </select>
+        </div> */}
+    </div>
+  );
+};
+
+export default Filters;
--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -1,24 +1,17 @@
 import React, { useEffect, useState } from "react";
 import { Fragment } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import {
-  getCountries,
-  orderAlphabetical,
-  orderContinent,
-  orderPopulation,
-  getActivities,
-  byActivity,
-  // forActivity,
-} from "../redux/actions";
+import { getCountries, getActivities } from "../redux/actions";
+
 import Card from "./Card";
 import { Link } from "react-router-dom";
 import Pagination from "./Pagination";
 import Search from "./Search";
+import Filters from "./Filters";
 
 export default function Home() {
   const dispatch = useDispatch();
   const countries = useSelector((state) => state.countries);
-  const activities = useSelector((state) => state.activities);
 
   const [currentPage, setCurrentPage] = useState(1);
   const countriesPerPage = 10;
@@ -40,33 +33,6 @@ export default function Home() {
     dispatch(getCountries());
   }
 
-  function handleOrderAbc(e) {
-    e.preventDefault();
-    dispatch(orderAlphabetical(e.target.value));
-    setCurrentPage(1);
-  }
-
-  function handlePopulation(e) {
-    e.preventDefault();
-    dispatch(orderPopulation(e.target.value));
-    setCurrentPage(1);
-  }
-
-  function handleContinent(e) {
-    e.preventDefault();
-    dispatch(orderContinent(e.target.value));
-  }
-
-  function handleFilteredActivity(e) {
-    e.preventDefault();
-    dispatch(byActivity(e.target.value));
-  }
-
-  //  function handleFilteredActivitySeason(e){
-  //     e.preventDefault();
-  //     dispatch(forActivity(e.target.value))
-  //   }
-
   useEffect(() => {
     dispatch(getCountries());
   }, [dispatch]);
@@ -86,60 +52,7 @@ export default function Home() {
           «THE WORLD IS A BOOK, AND THOSE WHO DON'T TRAVEL READ ONLY ONE PAGE»
         </h1>
       </div>
-      <div className="filtros">
-        <div className="ordertext">
-          <span className="inputfiltros">Order Alphabetical</span>
-          <select className="orderbutton" onChange={handleOrderAbc}>
-            <option value="ascendente">A-Z</option>
-            <option value="descendente">Z-A</option>
-          </select>
-        </div>
-        <div className="ordertext">
-          <span className="inputfiltros">Order Population</span>
-          <select className="orderbutton" onChange={handlePopulation}>
-            <option value="ascendente">Desc</option>
-            <option value="descendente">Asc</option>
-          </select>
-        </div>
-        <div className="ordertext">
-          <span className="inputfiltros">Order Continent</span>
-          <select className="orderbutton" onChange={handleContinent}>
-            <option value="all">All</option>
-            <option value="North America">North America</option>
-            <option value="South America">South America</option>
-            <option value="Antarctica">Antarctica</option>
-            <option value="Europe">Europe</option>
-            <option value="Asia">Asia</option>
-            <option value="Oceania">Oceania</option>
-          </select>
-        </div>
-        <div className="ordertext">
-          <span className="inputfiltros">Activities</span>
-          <select
-            className="orderbutton"
-            onChange={(e) => handleFilteredActivity(e)}
-          >
-            <option value="all">All</option>
-            {activities?.map((country) => {
-              return (
-                <option key={country.id} value={country.name}>
-                  {country.name}
-                </option>
-              );
-            })}
-          </select>
-        </div>
-        {/* <div className="ordertext">
-        <span className="inputfiltros">Activities</span>
-          <select className="orderbutton" onChange= {(e) => handleFilteredActivitySeason(e)}>
-                <option value="all">All</option>
-                     {activities?.map((country) => {
-                       return <option key={country.id} value={country.season} >{country.season}</option>
-                    })}
-                    
-         </select>
-        </div> */}
-      </div>
+      <Filters />
       <button
         className="reloadbutton"
         onClick={(e) => {
