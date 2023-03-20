697de817850a6586d7619932f0745449dc7d4f5e
Tateuer <tateuer@gmail.com>
2023-03-20 11:19:11 -0300

    ahora siii

--- api/src/app.js
+++ api/src/app.js
@@ -17,7 +17,7 @@ server.use(morgan("dev"));
 server.use((req, res, next) => {
   res.header(
     "Access-Control-Allow-Origin",
-    "https://countries-jvkqua3uu-tateuer-gmailcom.vercel.app"
+    "https://countries-pi-eta-ten.vercel.app"
   ); // update to match the domain you will make the request from
   res.header("Access-Control-Allow-Credentials", "true");
   res.header(
